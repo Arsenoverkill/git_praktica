72361450a5dad1f07b2c726afbefd58c72cb26a3
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-02-29 14:36:09 +0600

    header

---
+++ src/components/index.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import "./index.css"
+const Header = () => {
+    return (
+        <div className='header'>
+        <img src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcR7xYtcckMzGZTQvQ_CsLTtQRp98hdl32_KRUPyqBjvMw&s" alt="" />
+        <nav>
+            <a href="#">Home</a>
+            <a href="#">Order</a>
+            <a href="#">Admin</a>
+        </nav>
+        <button>create</button>
+        </div>
+    );
+};
+
+export default Header
