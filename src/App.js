7bd3977ecabf66281977dea09d054b329372904c
Arsenoverkill <147966182+Arsenoverkill@users.noreply.github.com>
2024-03-01 18:58:39 +0600

    footer (#2)

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import './App.css';
 import Header from './components';
+import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Footer/>
     </div>
   );
 }
