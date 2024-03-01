f53df20567c7606d69700279c3af0fe024755624
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-03-01 19:14:45 +0600

    center

--- src/App.js
+++ src/App.js
@@ -1,11 +1,13 @@
 import './App.css';
 import Header from './components';
+import Center from './components/Center';
 import Footer from './components/Footer';
 
 function App() {
   return (
     <div className="App">
      <Header/>
+     <Center/>
      <Footer/>
     </div>
   );
---
+++ src/components/Center.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Center = () => {
+    return (
+        <div>
+            <h1 style={{
+          textAlign: "center",
+          margin: "30px",
+        }}>Arsen 002</h1>
+        </div>
+    );
+};
+
+export default Center;
