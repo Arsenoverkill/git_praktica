039337e8254ec08647c843af1e71a03ca1175cda
Arsenoverkill <arsenprogrammer007@gmail.com>
2024-03-01 18:57:56 +0600

    footer

---
+++ src/components/Footer.jsx
@@ -0,0 +1,17 @@
+import React from "react";
+import "./Footer.css";
+
+const Footer = () => {
+  return (
+    <footer>
+      <p>Arsen</p>
+      <p>Arsen</p>
+      <p>Arsen</p>
+      <p>Arsen</p>
+      <p>Arsen</p>
+      <p>Arsen</p>
+    </footer>
+  );
+};
+
+export default Footer;
